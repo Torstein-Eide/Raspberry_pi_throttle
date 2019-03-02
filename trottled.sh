47018d0251c332eb9d4d0f3f1108a96f9db53ebf
Torstein Eide <torsteine@gmail.com>
2019-03-02 11:41:13 +0100

    show less info for status that is OK, and soft limit for raspberry 3B+

--- trottled.sh
+++ trottled.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 #before first run
 #run chmod +x ./trottled.sh
-if [ "$(whoami)" != "root" ]; then
-            echo "Script must be run as user: root"
-            sudo $0
-            sleep 1
-  fi
-
-
-#Text Colors
-GREEN=`tput setaf 2`
-RED=`tput setaf 1`
-NC=`tput sgr0` #No color
-if [[ $EUID -ne 0 ]]; then
-  echo -e "${RED}ERROR: You must be a root user${NC}" 2>&1
-  exit 1
-fi
-if hash bc  2>/dev/null; then
-        echo "bc installed"
-    else
-        echo "${RED}ERROR: bc not installed, ${NC}run 'sudo apt update; sudo apt install bc'"
-        exit
-    fi
-
 
 #Flag Bits
 UNDERVOLTED=0x1
@@ -31,12 +9,19 @@ THROTTLED=0x4
 HAS_UNDERVOLTED=0x10000
 HAS_CAPPED=0x20000
 HAS_THROTTLED=0x40000
+SOFT_TEMP_LIMIT=0x8
+HAS_SOFT_TEMP_LIMIT=0x80000
 
-
+#Text Colors
+GREEN=`tput setaf 2`
+RED=`tput setaf 1`
+NC=`tput sgr0` #No color
 
 #Output Strings
 GOOD="${GREEN}NO${NC}"
 BAD="${RED}YES${NC}"
+OK="${GREEN}OK${NC}"
+
 
 while true;
 do
@@ -44,10 +29,10 @@ do
 #Get Status, extract hex
 STATUS=$(vcgencmd get_throttled)
 STATUS=${STATUS#*=}
+
 #Get Temp, extract hex
 TEMP=$(vcgencmd measure_temp)
 TEMP=${TEMP#*=}
-
 
 ClockARM=$(vcgencmd measure_clock arm)
 ClockARM=$(bc <<< "${ClockARM#*=}/1000000")
@@ -59,30 +44,61 @@ VoltCore=${VoltCore#*=}
 
 clear;
 
-echo -n "Status: "
 # test, if true do red else grenn
-(($STATUS!=0)) && echo "${RED}${STATUS}${NC}" || echo "${GREEN}${STATUS}${NC}"
+echo -n "Status: "
+(($STATUS!=0)) && echo "${RED}${STATUS}${NC}" || echo "${GREEN}${STATUS}, no throttling or capping ${NC}"
 
-echo "Undervolted:"
-echo -n "   Now: "
-((($STATUS&UNDERVOLTED)!=0)) && echo "${BAD}" || echo "${GOOD}"
-echo -n "   Run: "
-((($STATUS&HAS_UNDERVOLTED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+if [ $STATUS!=0 ];
+	then
+	echo "Status: "
 
-echo "Throttled:"
-echo -n "   Now: "
-((($STATUS&THROTTLED)!=0)) && echo "${BAD}" || echo "${GOOD}"
-echo -n "   Run: "
-((($STATUS&HAS_THROTTLED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+	if ((($STATUS&HAS_UNDERVOLTED)!=0))
+		then
+			echo "Undervolted:"
+			echo -n "   Now: "
+			((($STATUS&UNDERVOLTED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+			echo -n "   Run: "
+			((($STATUS&HAS_UNDERVOLTED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+		else
+			echo "Undervolted:	${OK}"
+	fi
+	if 	((($STATUS&HAS_THROTTLED)!=0))
+		then
+			echo "Throttled:"
+			echo -n "   Now: "
+			((($STATUS&THROTTLED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+			echo -n "   Run: "
+			((($STATUS&HAS_THROTTLED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+		else
+			echo "Throttled:	${OK}"
+	fi
+	if ((($STATUS&HAS_CAPPED)!=0))
+	then
+		echo "Frequency Capped:"
+		echo -n "   Now: "
+		((($STATUS&CAPPED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+		echo -n "   Run: "
+		((($STATUS&HAS_CAPPED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+	else
+		echo "Freq Capped:	${OK}"
+	fi
 
-echo "Frequency Capped:"
-echo -n "   Now: "
-((($STATUS&CAPPED)!=0)) && echo "${BAD}" || echo "${GOOD}"
-echo -n "   Run: "
-((($STATUS&HAS_CAPPED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+	if ((($STATUS & HAS_SOFT_TEMP_LIMIT)!=0))
+		then
+			echo "Temp over 60c:"
+		echo -n "   Now: "
+		((($STATUS & SOFT_TEMP_LIMIT)!=0)) && echo "${BAD}" || echo "${GOOD}"
+		echo -n "   Run: "
+		((($STATUS & HAS_SOFT_TEMP_LIMIT)!=0)) && echo "${BAD}" || echo "${GOOD}"
+	else
+		echo "Temp over 60c:	${OK}"
+	fi
+
+fi
+
 
 echo  "ARM:	Core:	Core Voltage:	Core Temp:"
 echo  "${GREEN}${ClockARM}${NC}Mhz	${GREEN}${Clockcore}${NC}MHz	${GREEN}${VoltCore}${NC}		${GREEN}${TEMP}${NC}"
 
-sleep 1 
+sleep 1
 done
